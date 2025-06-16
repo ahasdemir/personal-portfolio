7a78ae6c4da187ccdb2b072e39e8ec480e1ef11d
Ahmet Hasdemir <ahmet@hasdemir.me>
2025-06-16 16:19:49 +0300

    Update base URL in sitemap to reflect new domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from 'app/blog/utils'
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = 'https://hasdemir.me'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
